74e9ad135ba6fe07c602c02d3ad3dff8b5cd16e4
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-09 16:45:50 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,17 @@
+package Dayy2;
+
+public class Dayy2 {
+    
+    public static void main(String[] args) {
+        
+        String nama = "fatin";
+        int umur = 18;
+        double Bb = 45.0;
+        
+        System.out.println("umur:"+umur);
+        System.out.print("nama:"+nama);
+        System.out.print("\n");
+        System.out.printf("nama saya %s, Bb saya %.1f, dan umur saya %dtahun.",nama,Bb,umur);
+    } 
+    
+}
