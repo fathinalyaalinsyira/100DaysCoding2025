d54a4fca2b255a3309c82f774be851089ae5483a
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-11 16:30:57 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,21 @@
+package Day4;
+
+public class main {
+    public static void main(String[] args) {
+        
+        System.out.println("\"Hello World\"");
+        System.out.println("Nama:\tfatin");
+        System.out.println("Umur:\t18tahun");
+        System.out.println("Jenis: \'P\'");
+        System.out.println("NIM D0225020\\teknik\\Informatika");
+        System.out.println("Saya adalah mahasiswa\nUniversitas Sulawesi Barat");
+        System.out.println("Terima \bkasih");
+        
+       // \n = new line (baris baru) digunakan untuk baris baru
+       // \t = tab (indentasi) digunakan untuk membuat jarak atau spasi tabulasi
+       // \b = backspase (menghapus karakter sebelumnya) digunakan untuk menghapus karakter atau spasi
+       // \  = backslash (mewakili karakter backslash) digunakan untuk menambahkan garis miring
+       // '  = single qoute (mewakili karakter single qoute) digunakan untuk menambahkan petik satu
+       // "  = double qoute (mewakili karakter double qoute) digunakan untuk menambahkan petik dua
+    }
+}
