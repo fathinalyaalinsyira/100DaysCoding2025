9bc4ab8bd118f124f7ad73241e75eeb8d33cf063
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-12 19:52:37 +0800

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,24 @@
+package Day5;
+
+public class main {
+    public static void main(String[] args) {
+        byte berat = 45;
+        short tinggi = 154;
+        long tahun = 2006;
+        int umur = 18;
+        
+        System.out.println("berat badan saya"+ berat);
+        System.out.println("tinggi badan saya"+ tinggi);
+        System.out.println("tahun lahir saya"+ tahun);
+        System.out.println("umur saya"+ umur);
+        
+    
+        //penjelasan mengenai tipe data
+        /*byte : tipe data yang digunakan untuk data dalam skala kecil.
+        /*short : tipe data dengan jangkauan yang lebih besar dari Byte.
+        /*long : tipe data untuk nilai yang melebihi kapasitas jangkauan tipe data Int,
+         *sehingga memiliki rentang yang jauh lebih tinggi. 
+        /*int : tipe data bilangan bulat yang paling umum.
+         */
+    }   
+}
