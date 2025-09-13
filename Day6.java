a77010991ea4111c9036d363bd24fdeb81d9f0c5
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-13 11:32:30 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,16 @@
+package Day6;
+
+public class main {
+    public static void main(String[] args) {
+        float berat = 45.0f;
+        double tinggi = 154.0;
+        
+        System.out.println(berat);
+        System.out.println(tinggi);
+        
+        // Penjelasan mengenai tipe data float dan double
+        /*float : tipe data yang digunakan untuk bilangan desimal dengan presisi tunggal.
+        /*double : tipe data primitif yang digunakan untuk bilangan desimal dengan presisi ganda, lebih presisi daripada float.
+        */
+    }
+}
