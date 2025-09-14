cd89c713623f7ad7c2b23928fa31fdcfaa96d8e8
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-14 10:32:39 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,17 @@
+package Day7;
+
+public class main {
+    public static void main(String[] args) {
+        char nilai = 'A';
+        boolean lulus = true;
+        
+        System.out.println("nilai"+ nilai);
+        System.out.println("lulus"+ true);
+        
+        //Penjelasan mengenai tipe data char dan boolean
+        /*char : tipe data yang digunakan untuk satu karakter atau tunggal
+        /*boolean : tipe data yang digunakan untuk membuat keputusan,true(benar) or false(salah)
+        */
+    
+}
+}
