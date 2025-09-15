58817daf03a5ce03e92ffdb4b3a97cbd29306ffa
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-15 11:10:53 +0800

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,24 @@
+package day8;
+
+public class main {
+    public static void main(String[] args) {
+        String nama = "fatin";
+        String umur = "19";
+        String makan = "mie ayam";
+        String asal = "Polewali";
+        String hobi = "makan";
+        String fakultas = "teknik";
+   
+        System.out.println("hallo nama saya "+nama);
+        System.out.println("saya berusia "+ umur + " tahun");
+        System.out.println("saya berasal dari "+ asal);
+        System.out.println("makanan favorit saya "+ makan);
+        System.out.println("hobi saya "+ hobi);
+        System.out.println("saya dari fakultas "+ fakultas);
+        
+        //Penjelasan mengenai tipe data String
+        /*String : tipe data yang digunakan untuk memanipulasi rangkaian karakter,
+        seperti teks, kata, kalimat, atau paragraf.
+        */
+    }
+}
