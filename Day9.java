d6d47b0fc2d724aac20848a9e51349844bfb3673
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-16 21:46:33 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,28 @@
+package Day9;
+
+public class main {
+    public static void main(String[] args) {
+        //Deklarasi 
+        String nama;
+        int umur;
+        
+        //Inisialisasi 
+        nama = "Fatin";
+        umur = 19;
+        System.out.println("Nama saya "+ nama);
+        System.out.println("Umur saya  "+ umur + "tahun");
+        
+        //Update variabel
+        nama = "Alin";
+        umur = 18;
+        System.out.println("Nama lain saya "+ nama);
+        System.out.println("Umur saya tahun lalu "+ umur +"tahun");
+        
+        //Penjelasan mengenai Deklarasi, Inisialisasi, Update Variabel
+        /*Deklarasi : menentukan variabel dan tipe data
+        /*Inisialisai : nilai awal pada variabel setelah dideklarasi
+        /*Update variabel : proses mengubah nilai yang sudah ada pada variabel
+        */
+        }
+}
+ 
