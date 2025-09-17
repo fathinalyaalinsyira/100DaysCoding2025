bd2904a39c8c22dd8e7d362812904dfa6d705a5e
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-17 12:46:18 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,11 @@
+package Day10;
+
+public class main {
+    public static void main(String[] args) {
+        final int berat = 45;
+        System.out.println("berat badan: "+ berat);
+        //Penjelasan mengenai Final Variabel
+        /*Final Variabel : variabel yang nilainya tidak dapat diubah setelah diinisialisasi
+        */
+    }
+}
