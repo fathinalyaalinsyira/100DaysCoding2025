6a9159a4c093fd1cf8383126556c0baebcd3cf78
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-25 20:18:45 +0800

    Create Day018.java

---
+++ Day018.java
@@ -0,0 +1,18 @@
+package Day18;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //Konverensi Otomatis (Tipe Data Kecil Ke Besar)
+        
+        //Wide Cast
+        int a = 19;
+        float b = a;
+        //secara otomatis a(int) berubah menjadi b(float)
+        System.out.println("int ke floata: "+ b);
+        
+       
+                
+    }
+}
