3575d1f4aca4d2524b4346ad32ccbba4a9c394ba
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-09-26 17:48:30 +0800

    Create Day019.java

---
+++ Day019.java
@@ -0,0 +1,17 @@
+package Day19;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        //Konverensi Manual/Paksa (Dari Tipe Data Besar Ke kecil)
+        
+        //type Cast
+        double a = 19.0;
+        int b = (int) a;
+        //secara otomatis a(double) berubah menjadi b(int)
+        System.out.println("double ke int:19.0= "+ b);
+        
+        
+       
+    }
+}
