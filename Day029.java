69d330810c64ecedd0e8cad9e74042255d448553
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-10-06 18:24:38 +0800

    < dan >

---
+++ Day029.java
@@ -0,0 +1,26 @@
+package Day29;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        //Operator Perbandingan < dan >
+
+        System.out.print("masukkan a: ");
+        int a = p.nextInt();
+
+        System.out.print("masukkan b: ");
+        int b = p.nextInt();
+
+        boolean hasil1 = (a < b);
+        boolean hasil2 = (a > b);
+
+        System.out.println("\nhasil perbandingan ");
+        System.out.println("apakah lebih kecil: " + hasil1);
+        System.out.println("apakah lebih besar: " + hasil2);
+
+    }
+
+}
