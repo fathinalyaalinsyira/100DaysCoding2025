d960ff61cf0e12d8f99e42cce828f5925cc9b6e2
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-10-18 18:45:46 +0800

    EVALUASI 2

---
+++ Day041.java
@@ -0,0 +1,44 @@
+package Day41;
+
+import java.util.Scanner;
+
+public class Main41 {
+
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+
+        // EVALUASI MENTOR 2
+        // SOAL 1
+//        System.out.print("Masukkan suhu dalam celcius: ");
+//        double suhu = p.nextDouble();
+//
+//        System.out.println("Pilih Konversi:");
+//        System.out.println("1. Fahrentheit");
+//        System.out.println("2. Kelvin");
+//        System.out.print("Masukkan pilihan anda: ");
+//        int pilih = p.nextInt();
+//
+//        if (pilih == 1) {
+//            double hasil = (suhu * 9 / 5) + 32;
+//            System.out.println("Hasil Konversi: " + suhu + "C =" + pilih + "f");
+//        } else {
+//            final double hasil = suhu + 273.15;
+//            System.out.println("Hasil Konversi: " + suhu + " = " + hasil + "k");
+//        }
+
+        // SOAL 2
+//          System.out.print("User: ");
+//          String user = p.nextLine();
+//          System.out.print("Password: ");
+//          String sandi = p.nextLine();
+//          
+//          if (user.equalsIgnoreCase("fatin") && sandi.equalsIgnoreCase("123")){
+//              System.out.println("Anda login sebagai pengguna");
+//          } else if (user.equalsIgnoreCase("admin") && sandi.equalsIgnoreCase("admin")){
+//              System.out.println("Anda login sebagai admin");
+//          } else {
+//              System.out.println("Akun tidak ditemukan");
+//          }
+    }
+
+}
