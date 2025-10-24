47b272aee5a03dda1c0b396eec3ce16c926144f6
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-10-24 19:45:22 +0800

    menentukan hari

---
+++ Day47.java
@@ -0,0 +1,39 @@
+package Day47;
+
+import java.util.Scanner;
+
+public class Main47 {
+
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+
+        //Program menentukan hari
+        System.out.print("Masukkan angka untuk hari: ");
+        int hari = p.nextInt();
+
+        switch (hari) {
+            case 1:
+                System.out.println("HARI SENIN");
+                break;
+            case 2:
+                System.out.println("HARI SELASA");
+                break;
+            case 3:
+                System.out.println("HARI RABU");
+                break;
+            case 4:
+                System.out.println("HARI KAMIS");
+                break;
+            case 5:
+                System.out.println("HARI JUM'AT");
+                break;
+            case 6:
+                System.out.println("HARI SABTU");
+            case 7:
+                System.out.println("HARI MINGGU");
+            default:
+                System.out.println("HARI TIDAK ADA");
+        }
+    }
+
+}
