c21c3b8b8247092e9e68bae1cd18f1f4059c23ad
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-10-26 16:00:25 +0800

    operator ternary

---
+++ Day049.java
@@ -0,0 +1,24 @@
+package Day49;
+
+import java.util.Scanner;
+
+public class Main49 {
+
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        //Operator Ternary
+
+        System.out.print("Masukkan nama: ");
+        String nama = p.nextLine();
+        System.out.print("Masukkan umur: ");
+        int umur = p.nextInt();
+
+        String status = (umur >= 19) ? "DEWASA" : "BOCIL";
+
+        System.out.println("====== KETERANGAN MAHASISWA ======");
+        System.out.println("Nama: " + nama);
+        System.out.println("Nilai: " + umur);
+        System.out.println("Status: " + status);
+    }
+
+}
