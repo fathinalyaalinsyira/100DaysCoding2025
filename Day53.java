967f61eebdcde0a7eb5fd920fa7b222e6e577d55
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-10-30 19:35:01 +0800

    keyword break

---
+++ Day53.java
@@ -0,0 +1,25 @@
+package Day53;
+
+import java.util.Scanner;
+
+public class Main53 {
+
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        //keyword break
+
+        System.out.print("Masukkan angka pertama: ");
+        int angka = p.nextInt();
+        System.out.print("Masukkan batas: ");
+        int batas = p.nextInt();
+
+        for (int i = angka; i < batas; i++) {
+            if (i == batas) {
+                break;
+            }
+            System.out.println("Perulangan ke- " + i);
+        }
+        System.out.println("Perulangan berhenti");
+    }
+
+}
