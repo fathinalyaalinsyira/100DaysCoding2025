483cb5d7c31e8bbb200bcaf7a69e3cec73dd80d2
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-11-07 19:55:29 +0800

    kelipatan m dari 1-n

---
+++ Day061.java
@@ -0,0 +1,23 @@
+package Day61;
+
+import java.util.Scanner;
+
+public class Main61 {
+    public static void main(String[] args) {
+        Scanner p = new Scanner (System.in);
+        //mencetak bilangan kelipatan M dari 1-N
+        
+        System.out.print("masukkan nilai N : ");
+        int N = p.nextInt();
+        System.out.print("masukkan kelipatan M : ");
+        int M = p.nextInt();
+        
+        System.out.println("\nkelipatan : "+ M +" dari "+ N);
+        for (int i = 1; i <= N; i++) {
+            if (i % M ==0) {
+                System.out.print(i +" ");
+            }
+        }
+            System.out.println();
+    }
+}
