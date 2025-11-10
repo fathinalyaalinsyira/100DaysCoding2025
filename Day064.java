760d422915b1bbccb70b977479f10cf1052539e7
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-11-10 15:51:29 +0800

    m pangkat n

---
+++ Day064.java
@@ -0,0 +1,23 @@
+package Day64;
+
+import java.util.Scanner;
+
+public class Main64 {
+
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        //mencetak hasil M pangkat N
+
+        System.out.print("masukkan M : ");
+        int M = p.nextInt();
+
+        System.out.print("masukkan N : ");
+        int N = p.nextInt();
+
+        int hasil = 1;
+        for (int i = 0; i < N; i++) {
+            hasil *= M;
+        }
+        System.out.println(M + " pangkat " + N + " = " + hasil);
+    }
+}
