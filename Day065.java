29d4b38d9bdbc0d48ddb9b9d097872043a42419d
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-11-11 18:33:09 +0800

    bilangan faktorial

---
+++ Day065.java
@@ -0,0 +1,20 @@
+package Day65;
+
+import java.util.Scanner;
+
+public class Main65 {
+
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        //membuat bilangan faktorial
+
+        System.out.print("masukkan bilangan : ");
+        int N = p.nextInt();
+
+        long faktorial = 1;
+        for (int i = 1; i <= N; i++) {
+            faktorial *= i;
+        }
+        System.out.println("faktorial dari " + N + " adalah: " + faktorial);
+    }
+}
