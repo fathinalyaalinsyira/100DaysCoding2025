939f2ceac59c18846d9b513793f5972bd762157f
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-11-15 18:47:15 +0800

    Evaluasi mentor 3

---
+++ Day069.java
@@ -0,0 +1,35 @@
+package Day69;
+
+import java.util.Scanner;
+
+public class EvaluasiMentor {
+
+    public static void main(String[] args) {
+        Scanner p = new Scanner(System.in);
+        //Evaluasi Mentor 3
+
+//        //SOAL 2
+//        System.out.print("jumlah karung: ");
+//        int jumlah = p.nextInt();
+//
+//        System.out.print("berat per karung: ");
+//        int perkarung = p.nextInt();
+//
+//        int total = 0;
+//
+//        total = jumlah * perkarung;
+//        System.out.println("Total berat: " + total);
+//
+//        //SOAL 4
+//        System.out.print("jumlah petak: ");
+//        int jumlah = p.nextInt();
+//        int hasil = 0;
+//
+//        for (int i = 1; i <= jumlah; i++) {
+//            System.out.print("luas petak ke - " + i + ": ");
+//            int luas = p.nextInt();
+//            hasil += luas;
+//        }
+//        System.out.println("total luas sawah: " + hasil);
+    }
+}
