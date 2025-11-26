87176d2c0af0b1a7ede80a4376aae91e7b13bb04
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-11-26 20:40:01 +0800

    Array D1

---
+++ Day080.java
@@ -0,0 +1,15 @@
+package Day80;
+
+public class Main80 {
+
+    public static void main(String[] args) {
+        //Array D1
+
+        String[] a = {"fatin", "rusmi", "samra", "serli"};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + " : index ke- " + i);
+        }
+    }
+
+}
