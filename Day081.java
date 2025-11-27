523a8eaa500f9f6aefcca18093f84a14f4432747
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-11-27 23:14:16 +0800

    array dgn indeks

---
+++ Day081.java
@@ -0,0 +1,16 @@
+package Day81;
+
+public class Main81 {
+
+    public static void main(String[] args) {
+        //akses elemen array dengan index
+
+        String[] a = {"fatin", "rusmi", "samra", "serli"};
+
+        System.out.println("indeks ke 0: " + a[0]);
+        System.out.println("indeks ke 1: " + a[1]);
+        System.out.println("indeks ke 2: " + a[2]);
+        System.out.println("indeks ke 3: " + a[3]);
+    }
+
+}
