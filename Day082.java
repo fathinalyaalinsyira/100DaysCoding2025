7f6288a3d97fa84b458e710580e3821d5cc0f445
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-11-28 20:30:38 +0800

    mengisi dan menampilkan elemen array (loop)

---
+++ Day082.java
@@ -0,0 +1,15 @@
+package Day82;
+
+public class Main82 {
+
+    public static void main(String[] args) {
+        //mengisi elemen array dan menampilkan semua elemen array dengan perulangan
+
+        char[] a = {'a', 'b', 'c', 'd', 'e', 'f'};
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i] + " = index ke : " + i);
+        }
+    }
+
+}
