7a07bb6eca4bcbc8d5566a79dd82f736f981a545
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-12-04 18:42:47 +0800

    mengakses elemen array dengan menggunakan For-each

---
+++ Day088.java
@@ -0,0 +1,15 @@
+package Day88;
+
+public class Main88 {
+
+    public static void main(String[] args) {
+        //mengakses elemen array dengan menggunakan For-each
+
+        String[] a = {"fatin", "serli", "rusmi", "samra"};
+
+        for (String i : a) {
+            System.out.println(i);
+        }
+    }
+
+}
