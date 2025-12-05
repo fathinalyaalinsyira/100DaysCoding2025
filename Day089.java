c4ce8ec033cc158fdce51866db8a595e2dbd6526
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-12-05 19:23:24 +0800

    method void

---
+++ Day089.java
@@ -0,0 +1,14 @@
+package Day89;
+
+public class Main89 {
+
+    public static void main(String[] args) {
+        //Method void
+
+        nama();
+    }
+
+    public static void nama() {
+        System.out.println("hello world");
+    }
+}
