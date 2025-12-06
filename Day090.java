b67cf7a02f5338ec3ee92b39ffe8b3715f62c992
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-12-06 19:45:06 +0800

    method non-void

---
+++ Day090.java
@@ -0,0 +1,15 @@
+package Day90;
+
+public class Main90 {
+
+    public static void main(String[] args) {
+        //method non void
+        System.out.println(umur());
+    }
+
+    public static int umur() {
+        return 19;
+    }
+
+}
+ jelaskan
