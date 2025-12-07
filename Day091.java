0c806ddc7357d6c7d7fb9b0dec4f466f2c023939
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-12-07 21:38:26 +0800

    parameter method

---
+++ Day091.java
@@ -0,0 +1,14 @@
+package Day91;
+
+public class Main91 {
+
+    public static void main(String[] args) {
+        //parameter method
+        nama("fatin");
+
+    }
+
+    public static void nama(String me) {
+        System.out.println("hallo nama saya " + me);
+    }
+}
