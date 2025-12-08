bcbf0b978b2a592434c040931eb75c174334d883
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-12-08 18:52:01 +0800

    keyword return

---
+++ Day092.java
@@ -0,0 +1,13 @@
+package Day92;
+
+public class Main92 {
+
+    public static void main(String[] args) {
+        //keyword return
+        System.out.println(angka());
+    }
+
+    public static int angka() {
+        return 19;
+    }
+}
