b0cc108fbb8f05b288be405ca28f005b244a24be
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-12-10 21:10:44 +0800

    mencetak method void

---
+++ Day094.java
@@ -0,0 +1,14 @@
+package Day94;
+
+public class Main94 {
+
+    public static void main(String[] args) {
+        //mencetak dengan method void
+        cetak();
+    }
+
+    public static void cetak() {
+        System.out.println("Hello World");
+    }
+
+}
