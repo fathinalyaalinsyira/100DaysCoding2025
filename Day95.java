1236467fa1f9e447f3b49bc1f092dc22c50617f8
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-12-11 19:51:05 +0800

    parameter void

---
+++ Day95.java
@@ -0,0 +1,13 @@
+package Day95;
+
+public class Main95 {
+
+    public static void main(String[] args) {
+        //mencetak dengan parameter void
+        namaParameter("fatin");
+    }
+
+    public static void namaParameter(String nama) {
+        System.out.println("nama saya " + nama);
+    }
+}
