3860d5bce0eccb6f1d5da152709f8e70425157df
fathinalyaalinsyira <fatinayyub@gmail.com>
2025-12-12 20:57:22 +0800

    menghitung method non-void

---
+++ Day096.java
@@ -0,0 +1,29 @@
+package Day96;
+
+public class Main96 {
+
+    public static void main(String[] args) {
+        //menghitung penjumlahan, pengurangan, perkalian, dan pembagian dengan method non-void
+
+        System.out.println(penjumlahan());
+        System.out.println(pengurangan());
+        System.out.println(perkalian());
+        System.out.println(pembagian());
+    }
+
+    public static int penjumlahan() {
+        return 1 + 2;
+    }
+
+    public static int pengurangan() {
+        return 1 - 2;
+    }
+
+    public static int perkalian() {
+        return 1 * 2;
+    }
+
+    public static int pembagian() {
+        return 1 / 2;
+    }
+}
